4a84d7c10383a7ca32696168fe5e2c043846aabb
Maksym Klymyshyn <klymyshyn@gmail.com>
2015-05-16 12:21:03 +0300

    Fixed tests and initial version of README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Isoroutes
+
+.. image:: https://api.travis-ci.org/joymax/isoroutes.svg
+
+Library to everyday needs related to isomorphic development with React.js and
+any server-ready JavaScript framework.
+
+
+##  FAQ
+
+1. **Why another router?**
+
+	I believe that it’s possible to make router functionality pure and simple.
+	Without promises and bunch of callback-style code. Right now it’s completely pure:
+	for same input (*state*) router generate same output (*rendered markup*).
+
+
+1. **Why CSP channels?**
+
+	With CSP channels really easy organize asynchronous workflow which required for
+	retreiving multiple components state. Code looks clean, testable and easy-to-understand.
+
+
+1. **It's only for React.js?**
+
+	No, it's possible to use with **any server-ready** framework (even vanilla JS!).
+	Just write correspond renderer and move on.
--- test/test_react.js
+++ test/test_react.js
@@ -13,100 +13,100 @@ var stripattrs = require("./helpers").stripReactAttrs;
 
 // sample component
 var componentFactory = function (displayName, stateFunc) {
-    return React.createClass({
-        displayName: displayName,
-        statics: {
-            state: stateFunc
-        },
-        render: function () {
-            return <h1>Hello, <span>{this.props.title}</span>!</h1>
-        }
-    })
+  return React.createClass({
+    displayName: displayName,
+    statics: {
+      state: stateFunc
+    },
+    render: function () {
+      return <h1>Hello, <span>{this.props.title}</span>!</h1>
+    }
+  })
 };
 
 var Hello1 = componentFactory("Hello1", function (state, channel, cid) {
-    return function * () {
-        yield put(channel, ["title", "World"]);
-        channel.close();
-    }
+  return go(function * () {
+    yield put(channel, ["title", "World"]);
+    channel.close();
+  })
 });
 var Hello2 = componentFactory("Hello2", function (state, channel, cid) {
-    return function * () {
-        yield put(channel, ["title", "John"]);
-        channel.close();
-    }
+  return go(function * () {
+    yield put(channel, ["title", "John"]);
+    channel.close();
+  })
 });
 
 describe("React rendering", () => {
-    it("should render Hello component", function(done) {
-        var ch = chan();
-        var routes = router([
-            ["/", react(Hello1)]
-        ]);
+  it("should render Hello component", function(done) {
+    var ch = chan();
+    var routes = router([
+      ["/", react(Hello1)]
+    ]);
 
-        var result = routes(state.server({"url": "/"}), ch);
-
-        go(function * () {
-            var data = yield take(ch);
-            var rendered = stripattrs(React.renderToString(
-                React.createFactory(Hello1)({title: "World"})));
-
-            assert(
-                stripattrs(data.get(keys.render).server(data)) === rendered,
-                "Should be rendered into React.js string");
-
-            done();
-        });
+    var result = routes(state.server({"url": "/"}), ch);
+    
+    go(function * () {
+      var data = yield take(ch);
+      var rendered = stripattrs(React.renderToString(
+        React.createFactory(Hello1)({title: "World"})));
+      
+      assert(
+        stripattrs(data.get(keys.render).server(data)) === rendered,
+        "Should be rendered into React.js string");
+      
+      done();
     });
+  });
 
-    it("should able to render multiple components", function(done) {
-        var ch = chan();
-        var routes = router([
-            ["/", react(Hello1, Hello2)]
-        ]);
-
-        var result = routes(state.server({"url": "/"}), ch);
-
-        go(function * () {
-            var data1 = yield take(ch);
-            var data2 = yield take(ch);
-
-            var rendered1 = stripattrs(React.renderToString(
-                React.createFactory(Hello1)({title: "World"})));
-            var rendered2 = stripattrs(React.renderToString(
-                React.createFactory(Hello2)({title: "John"})));
-
-            assert(
-                stripattrs(data1.get(keys.render).server(data1)) === rendered1,
-                "Should be rendered into React.js string " + rendered1);
-            assert(
-                stripattrs(data2.get(keys.render).server(data2)) === rendered2,
-                "Should be rendered into React.js string" + rendered2);
-
-            done();
-        });
+  it("should able to render multiple components", function(done) {
+    var ch = chan();
+    var routes = router([
+      ["/", react(Hello1, Hello2)]
+    ]);
+    
+    var result = routes(state.server({"url": "/"}), ch);
+    
+    go(function * () {
+      var data1 = yield take(ch);
+      var data2 = yield take(ch);
+      
+      var rendered1 = stripattrs(React.renderToString(
+        React.createFactory(Hello1)({title: "World"})));
+      var rendered2 = stripattrs(React.renderToString(
+        React.createFactory(Hello2)({title: "John"})));
+      
+      assert(
+        stripattrs(data1.get(keys.render).server(data1)) === rendered1,
+        "Should be rendered into React.js string " + rendered1);
+      assert(
+        stripattrs(data2.get(keys.render).server(data2)) === rendered2,
+        "Should be rendered into React.js string" + rendered2);
+      
+      done();
     });
+  });
 
-    it("should be able to collect states from multiple components", function (done) {
-        var ch = chan(1);
-        var routes = router([
-            ["/", react(Hello1, Hello2)]
-        ]);
+  it("should be able to collect states from multiple components", function (done) {
+    var ch = chan(1);
+    var routes = router([
+      ["/", react(Hello1, Hello2)]
+    ]);
 
-        assert(collect(state.server({"url": "/"}), routes, ch) === true,
-               "Page should exist");
-
-        go(function * () {
-            var data = yield take(ch);
-            var state = {
-                0: data.filter(e => e.get("id") === 0).first(),
-                1: data.filter(e => e.get("id") === 1).first()
-            }
-            assert(state[0].get("state").get("title") === "World",
-                   "First component should have title World");
-            assert(state[1].get("state").get("title") === "John",
-                   "Second component should have title John");
-            done();
-        });
+    assert(collect(state.server({"url": "/"}), routes, ch) === true,
+           "Page should exist");
+    
+    go(function * () {
+      var data = yield take(ch);
+      var state = {
+        0: data.filter(e => e.get("id") === 0).first(),
+        1: data.filter(e => e.get("id") === 1).first()
+      }
+      assert(state[0].get("state").get("title") === "World",
+             "First component should have title World");
+      assert(state[1].get("state").get("title") === "John",
+             "Second component should have title John");
+      done();
     });
+  });
 });
